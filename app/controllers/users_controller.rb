03e51f021a15434f64cd47b40c65d035a3561013
tzvaita <tzvaita@gmail.com>
2020-04-14 23:10:54 +0200

    Added editing functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
