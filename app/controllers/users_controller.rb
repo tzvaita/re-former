c6a57221dffa2350d27f836c1bd4f051728bd346
tzvaita <tzvaita@gmail.com>
2020-04-14 22:09:22 +0200

    New user form done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
